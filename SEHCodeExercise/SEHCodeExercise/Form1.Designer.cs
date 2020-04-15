2ada623953593b5d89d7d1cd39d4cb157b717b4c
hoang phan <hphan7676@gmail.com>
2020-04-15 11:19:23 -0700

    upload project

---
+++ SEHCodeExercise/SEHCodeExercise/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace SEHCodeExercise
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.textTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.Create = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(240, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Text";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(35, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 2;
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.Location = new System.Drawing.Point(90, 24);
+            this.titleTextBox.Multiline = true;
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(108, 45);
+            this.titleTextBox.TabIndex = 3;
+            // 
+            // textTextBox
+            // 
+            this.textTextBox.Location = new System.Drawing.Point(344, 27);
+            this.textTextBox.Multiline = true;
+            this.textTextBox.Name = "textTextBox";
+            this.textTextBox.Size = new System.Drawing.Size(113, 42);
+            this.textTextBox.TabIndex = 4;
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(552, 31);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(75, 23);
+            this.searchButton.TabIndex = 5;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // Create
+            // 
+            this.Create.Location = new System.Drawing.Point(621, 201);
+            this.Create.Name = "Create";
+            this.Create.Size = new System.Drawing.Size(75, 23);
+            this.Create.TabIndex = 6;
+            this.Create.Text = "Create";
+            this.Create.UseVisualStyleBackColor = true;
+            this.Create.Click += new System.EventHandler(this.Create_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(611, 384);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 7;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(38, 122);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(511, 394);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(750, 528);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.Create);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.textTextBox);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox textTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Button Create;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ SEHCodeExercise/SEHCodeExercise/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.PowerPoint;
+using Microsoft.Office.Core;
+using System.Collections.Generic;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace SEHCodeExercise
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {          
+            InitializeComponent();
+        }
+
+        ImageList imgs = new ImageList();
+        void LoadImageList()
+        {
+            imgs.ImageSize = new Size(100, 100);
+            for (int i = 0; i < srcList.Count; i++)
+            {
+                try
+                {
+                    WebClient wc = new WebClient();
+                    byte[] imageByte = wc.DownloadData(srcList[i]);
+                    MemoryStream ms = new MemoryStream(imageByte);
+                    Image img = Image.FromStream(ms);
+                    imgs.Images.Add(img);
+                    listView1.Items.Add((i + 1).ToString());
+                }
+                catch (Exception ex) { continue; }
+            }
+        }
+        void LoadListView()
+        {
+            LoadImageList();
+            listView1.GridLines = true;
+            listView1.LargeImageList = imgs;
+            for (int i = 0; i < srcList.Count; i++)
+            {
+                ListViewItem item = new ListViewItem();
+                item.ImageIndex = i;
+            }
+        }
+
+        List<string> srcList = new List<string>();
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            string userinput = titleTextBox.Text + " " + textTextBox.Text;
+                if (userinput==" ")
+                {
+                label3.Text = "Please enter text/title";
+                return;
+            }
+            label3.Text = " ";
+           
+            string tempURL = "http://images.google.com/search?q=" + userinput + "&tbm=isch&site=imghp";
+            string htmlCode;
+            WebClient client = new WebClient();
+            htmlCode = client.DownloadString(tempURL);
+            Match m;
+            string HRefPattern = @"<img.*?src=""(.*?)\""";
+           try
+            {
+                m = Regex.Match(htmlCode, HRefPattern,
+                                RegexOptions.IgnoreCase | RegexOptions.Compiled,
+                                TimeSpan.FromSeconds(1));
+                while (m.Success)
+                {
+                    m = m.NextMatch();
+                    srcList.Add(m.Groups[1].Value);
+                }
+            }
+            catch (RegexMatchTimeoutException reg)
+            {
+                MessageBox.Show(reg.ToString());
+            }
+            LoadListView();
+        }
+        private void Create_Click(object sender, EventArgs e)
+        {
+             // Create a new PowerPoint File      
+            Microsoft.Office.Interop.PowerPoint.Application pptFile = new Microsoft.Office.Interop.PowerPoint.Application();
+            Presentation pptPresentation = pptFile.Presentations.Add(MsoTriState.msoTrue);
+            CustomLayout customLayout = pptPresentation.SlideMaster.CustomLayouts[PpSlideLayout.ppLayoutText];
+
+            // Create the first slide
+            Slides firstSlide;
+            _Slide slide;
+            TextRange objText;
+            firstSlide = pptPresentation.Slides;
+            slide = firstSlide.AddSlide(1, customLayout);
+
+            // Add title from title textbob
+            objText = slide.Shapes[1].TextFrame.TextRange;
+            objText.Text = titleTextBox.Text;
+            objText.Font.Name = "Arial";
+            objText.Font.Size = 48;
+
+            // Add content from text textbox
+            objText = slide.Shapes[2].TextFrame.TextRange;
+            objText.Text = textTextBox.Text;
+            objText.Font.Size = 28;
+
+            // create new slides for images
+            for (int i = 0; i < imgIndex.Count; i++)
+            {
+                Slides iSlide;
+                _Slide picSlide;
+                TextRange picRange;
+                iSlide = pptPresentation.Slides;
+                picSlide = iSlide.AddSlide(2, customLayout);
+
+                picRange = picSlide.Shapes[1].TextFrame.TextRange;
+                Microsoft.Office.Interop.PowerPoint.Shape shape = picSlide.Shapes[1];
+
+                string url = srcList[imgIndex[i]];
+
+                string filename = Path.GetFileNameWithoutExtension(Path.GetRandomFileName()) + ".png";
+                string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                try
+                {
+                    WebClient client = new WebClient();
+                    client.DownloadFile(url, filePath + "\\" + filename);
+                    string picFile = filePath + "\\" + filename;             
+                    picSlide.Shapes.AddPicture(picFile, MsoTriState.msoFalse, MsoTriState.msoTrue, shape.Left, shape.Top, shape.Width, shape.Height);
+
+                }
+                catch (Exception ex) { }         
+            }
+          
+        }
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            srcList.Clear();
+            System.Windows.Forms.Application.Exit();
+        }
+
+        List<int> imgIndex = new List<int>();
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            imgIndex.Clear();
+            ListView lw = sender as ListView;
+            if (lw.SelectedItems.Count > 0)
+            {
+                foreach (ListViewItem item in lw.SelectedItems)
+                {
+                    imgIndex.Add(Int32.Parse(item.Text));
+                }
+            }
+        }
+    }
+}
